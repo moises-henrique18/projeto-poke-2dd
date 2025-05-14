ea8b534894f761111918475d7bf0dff238822676
programadormovel <programadormovel@gmail.com>
2025-05-14 08:31:06 -0300

    flexbox

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,23 @@
 import poke from './assets/pokeapi_256.png'
+import './Home.css'
 
 const Home = () => {
 
     return (
         <div>
-            <img src={poke} />
-            <img src={poke} />
-            <img src={poke} />
-            <img src={poke} />
-            <img src={poke} />
-            <img src={poke} />
+          
+            <div>
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+                <img src={poke} />
+            </div>
+            
         </div>
     )
 }
