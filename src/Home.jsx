71ba969e5a894ce45983c3adb97eada92c442578
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-05-22 13:01:26 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,20 @@
 import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
 import './Home.css'
-
 const Home = () => {
-
     return (
         <div>
-          
-            <div>
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-                <img src={poke} />
-            </div>
-            
-        </div>
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+        </div>           
     )
 }
-
 export default Home
