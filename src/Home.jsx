d069d56d671d4f5d0c2a546989b06db69d137cf2
Moisés <rm95087@estudante.fieb.edu.br>
2025-06-04 11:13:29 +0000

    grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,12 @@ import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
 import PokeFlex from "./PokeFlex"
+import PokeGrid from "./PokeGrid"
 const Home = () => {
     return (
         <div>
             <PokeFlex/>
+            <PokeGrid/>
         </div>           
     )
 }
